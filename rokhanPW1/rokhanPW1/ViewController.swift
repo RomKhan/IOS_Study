94b7a796dc6f685d624c96b0a59b538b26a0f8d0
Roman <roman_khan@MacBook-Air-Roman.local>
2021-09-13 04:10:24 +0300

    V2

---
+++ rokhanPW1/rokhanPW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rokhanPW1
+//
+//  Created by Roman on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
