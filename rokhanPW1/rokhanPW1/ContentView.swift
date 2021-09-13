267f7a917e058ef0b14bdc79b89fe0610d5287ba
Roman <roman_khan@MacBook-Air-Roman.local>
2021-09-13 03:39:00 +0300

    first commit

---
+++ rokhanPW1/rokhanPW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rokhanPW1
+//
+//  Created by Roman on 13.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rokhanPW1/rokhanPW1/rokhanPW1App.swift
@@ -0,0 +1,17 @@
+//
+//  rokhanPW1App.swift
+//  rokhanPW1
+//
+//  Created by Roman on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct rokhanPW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
