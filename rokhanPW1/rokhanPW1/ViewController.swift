7cde0f34cb18f9085b4c68f2cae84b1054cc23dc
Roman <roman_khan@MacBook-Air-Roman.local>
2021-09-14 23:12:47 +0300

    f

--- rokhanPW1/rokhanPW1/ViewController.swift
+++ rokhanPW1/rokhanPW1/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var views: [UIView]!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.layer.cornerRadius = 10
+        
+        super.view.bringSubviewToFront(button)
     }
 
+    var countOfVisible = 0
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
 
+        
+        for view in views {
+            view.frame = CGRect(x: CGFloat(arc4random_uniform(UInt32(self.view.frame.width))), y: CGFloat(arc4random_uniform(UInt32(self.view.frame.height))), width: CGFloat(Int.random(in: 10...200)), height: CGFloat(Int.random(in: 10...200)))
+            
+            let random = Int.random(in: 0...1)
+            if random == 0 && countOfVisible>6 && view.isHidden != true {
+                view.isHidden = true
+                countOfVisible -= 1
+            }
+            else if random == 1 && view.isHidden != false {
+                view.isHidden = false
+                countOfVisible += 1
+            }
+        }
+        
+        var set = Set<UIColor>()
+        while set.count < views.count {
+        set.insert(
+        UIColor(
+        red: .random(in: 0...1),
+        green: .random(in: 0...1),
+        blue: .random(in: 0...1),
+        alpha: 1))}
+        
+        for view in views {
+            view.layer.cornerRadius = 10
+        view.backgroundColor = set.popFirst()
+        }
+    }
+    
 }
 
