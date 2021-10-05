cdbf5edd4e0aa93c8e0a741f7eec2fea42ce4ed2
Roman Khan <rokhan@edu.hse.ru>
2021-10-06 01:50:09 +0300

    HW3 4 points

--- rokhanPW3/rokhanPW3/StackViewController.swift
+++ rokhanPW3/rokhanPW3/StackViewController.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class StackViewController: UIViewController {
     let stackView = UIStackView()
+    let scroll = UIScrollView()
     
     private var collection: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        setupScroll()
         setupStackView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        scroll.contentSize = CGSize(
+            width: self.view.frame.width,
+            height: stackView.frame.height
+        )
+    }
+    
+    private func setupScroll() {
+        scroll.alwaysBounceVertical = true
+        view.addSubview(scroll)
+        
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        scroll.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scroll.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        scroll.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scroll.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
     
     private func setupStackView() {
@@ -23,12 +45,11 @@ class StackViewController: UIViewController {
         stackView.alignment = .fill
         stackView.spacing = 5
         
-        view.addSubview(stackView)
-        
+        scroll.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
         addAlarmsToStack()
     }
     
