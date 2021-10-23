4f459fe08a1d0c4c0185cf92543e313506335691
Roman Khan <rokhan@edu.hse.ru>
2021-10-23 06:21:10 +0300

    HW4 4 points

---
+++ rokhanPW4/rokhanPW4/NoteModel.swift
@@ -0,0 +1,13 @@
+//
+//  NoteModel.swift
+//  rokhanPW4
+//
+//  Created by Roman on 23.10.2021.
+//
+
+import Foundation
+
+struct Note {
+    let title: String
+    let description: String
+}
--- rokhanPW4/rokhanPW4/NoteViewController.swift
+++ rokhanPW4/rokhanPW4/NoteViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 class NoteViewController : UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var textField: UITextField!
+    var outputViewController: ViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTabSaveNote(button:)))
+    }
+    
+    @objc
+    func didTabSaveNote(button: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated: true)
     }
     
     
